e36046d6a947c6425a11f1545dc379db9b769843
Bruno Ricardo <brunomenezes1511@gmail.com>
2023-07-03 16:35:49 -0300

    Implementação e criação de novas classes

--- src/model/Estacionamento.java
+++ src/model/Estacionamento.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Estacionamento {
 	
-	private Hora[] horarioFuncionamento;
+	private List<Hora> horarioFuncionamento;
 	private List<Acesso> acessos;
 	
 	private int capacidade;
@@ -13,31 +13,80 @@ public class Estacionamento {
 	private boolean situacaoCapacidade;
 	
 	
-	public Estacionamento() {
-		//
+	private Estacionamento() {
+		acessos = new LinkedList<Acesso>();
+		horarioFuncionamento = new LinkedList<Hora>();
 	}
 	
-	public Estacionamento(String nome, int cap, double retorno, boolean permissaoEntrada ) {
-		
+	public Estacionamento(int cap, double retorno) {
+		acessos = new LinkedList<Acesso>();
+		horarioFuncionamento = new LinkedList<Hora>();
+		this.capacidade = cap;
+		this.retornoContratante = retorno;
 	}
 	
-	public boolean setSituacaoCapacidade(boolean s ) {
+	public Estacionamento(int cap, double retorno, boolean permissaoEntrada ) {
+		acessos = new LinkedList<Acesso>();
+		horarioFuncionamento = new LinkedList<Hora>();
+		this.capacidade = cap;
+		this.retornoContratante = retorno;
+		this.situacaoCapacidade = permissaoEntrada;
+	}
+	
+	public boolean setSituacaoCapacidade(boolean s) {
 		this.situacaoCapacidade = s;
 		return situacaoCapacidade;
 	}
 	
 	public double calcularTotalContratante() {
-		pesquisarAcesso();
-		return retornoContratante;
+		List<Acesso> lista = getAcessos();
+		
+		double resultadoFinal = 0;
+		double valorReal;
+		
+		for(Acesso v: lista) {
+			resultadoFinal += v.getValorAcesso();
+		}
+		
+		valorReal = (resultadoFinal*retornoContratante)/100;
+		
+		return valorReal;
 	}
 	
-	public Acesso pesquisarAcesso(String placaCarro) {
-		for (Acesso a : acessos) {
-			if (a.getPlacaCarro() == placaCarro ) {
-				return a;
+	public void setHorarioFuncionamento(int hI, int mI, int hF, int mF) {
+		Hora inicio = new Hora(hI, mI);
+		Hora fim = new Hora(hF, mF);
+		
+		if(horarioFuncionamento.size() == 0 ) {
+			horarioFuncionamento.add(inicio);
+			horarioFuncionamento.add(fim);
+		} else {
+			horarioFuncionamento.clear();
+			horarioFuncionamento.add(inicio);
+			horarioFuncionamento.add(fim);
+		}
+	}
+	
+	public List<Hora> getHorarioFuncionamento() {
+		return horarioFuncionamento;
+	}
+	
+	public void setAcessos(String placa) {
+		Acesso veiculo = new Acesso(placa);
+		
+		acessos.add(veiculo);
+	}
+	
+	public void removeAcessos(String placa) {
+		for(Acesso v: acessos) {
+			if(v.getPlacaCarro() == placa) {
+				acessos.remove(v);
 			}
 		}
-		return null;
 	}
-
+	
+	public List<Acesso> getAcessos() {
+		return acessos;
+	}
+	
 }
